a9f5ba281ba6e3d8658948f2051d5b95a40650a1
John Lyon-Smith <john@lyon-smith.org>
2018-04-14 04:32:38 -0700

    Get object showing properly

--- App.js
+++ App.js
@@ -15,11 +15,11 @@ import {
 import autobind from "autobind-decorator"
 import { viroAPIKey } from "./keys"
 
-const distance = (vectorOne, vectorTwo) => {
+const distance = (vectorA, vectorB) => {
   var distance = Math.sqrt(
-    (vectorTwo[0] - vectorOne[0]) * (vectorTwo[0] - vectorOne[0]) +
-      (vectorTwo[1] - vectorOne[1]) * (vectorTwo[1] - vectorOne[1]) +
-      (vectorTwo[2] - vectorOne[2]) * (vectorTwo[2] - vectorOne[2])
+    (vectorB[0] - vectorA[0]) * (vectorB[0] - vectorA[0]) +
+      (vectorB[1] - vectorA[1]) * (vectorB[1] - vectorA[1]) +
+      (vectorB[2] - vectorA[2]) * (vectorB[2] - vectorA[2])
   )
   return distance
 }
@@ -37,63 +37,68 @@ class InitialARScene extends React.Component {
   }
 
   @autobind
-  handleLoadEnded() {
-    this.arScene
-      .getCameraOrientationAsync()
-      .then((orientation) => {
-        return this.refs.performARHitTestWithRay(orientation.forward)
-      })
-      .then((results) => {
-        // Default position is just 1.5 meters in front of the user.
-        let newPosition = [forward[0] * 1.5, forward[1] * 1.5, forward[2] * 1.5]
-        let hitResultPosition = undefined
+  handleLoadEnd() {
+    this.arScene.getCameraOrientationAsync().then((orientation) => {
+      return this.arScene
+        .performARHitTestWithRay(orientation.forward)
+        .then((results) => {
+          // Default position is just 1.5 meters in front of the user.
+          const forward = orientation.forward
+          const defaultPosition = [
+            forward[0] * 1.5,
+            forward[1] * 1.5,
+            forward[2] * 1.5,
+          ]
+          let hitResultPosition = null
 
-        // Filter the hit test results based on the position.
-        if (results.length > 0) {
-          for (var i = 0; i < results.length; i++) {
-            let result = results[i]
-            if (result.type == "ExistingPlaneUsingExtent") {
-              var distance = Math.sqrt(
-                (result.transform.position[0] - position[0]) *
-                  (result.transform.position[0] - position[0]) +
-                  (result.transform.position[1] - position[1]) *
-                    (result.transform.position[1] - position[1]) +
-                  (result.transform.position[2] - position[2]) *
-                    (result.transform.position[2] - position[2])
-              )
-              if (distance > 0.2 && distance < 10) {
-                // If we found a plane greater than .2 and less than 10 meters away then choose it!
-                hitResultPosition = result.transform.position
-                break
-              }
-            } else if (result.type == "FeaturePoint" && !hitResultPosition) {
-              // If we haven't found a plane and this feature point is within range, then we'll use it
-              // as the initial display point.
-              var distance = this._distance(position, result.transform.position)
-              if (distance > 0.2 && distance < 10) {
-                hitResultPosition = result.transform.position
+          // Filter the hit test results based on the position.
+          if (results.length > 0) {
+            for (var i = 0; i < results.length; i++) {
+              let result = results[i]
+
+              if (result.type == "ExistingPlaneUsingExtent") {
+                let distance = Math.sqrt(
+                  (result.transform.position[0] - position[0]) *
+                    (result.transform.position[0] - position[0]) +
+                    (result.transform.position[1] - position[1]) *
+                      (result.transform.position[1] - position[1]) +
+                    (result.transform.position[2] - position[2]) *
+                      (result.transform.position[2] - position[2])
+                )
+                if (distance > 0.2 && distance < 10) {
+                  // If we found a plane greater than .2 and less than 10 meters away then choose it!
+                  hitResultPosition = result.transform.position
+                  break
+                }
+              } else if (result.type == "FeaturePoint" && !hitResultPosition) {
+                // If we haven't found a plane and this feature point is within range, then we'll use it
+                // as the initial display point
+                let distance = this._distance(
+                  position,
+                  result.transform.position
+                )
+
+                if (distance > 0.2 && distance < 10) {
+                  hitResultPosition = result.transform.position
+                }
               }
             }
           }
-        }
 
-        if (hitResultPosition) {
-          newPosition = hitResultPosition
-        }
+          this.setState({
+            position: hitResultPosition || defaultPosition,
+          })
 
-        // Set the initial placement of the object using new position from the hit test.
-        this.setState({
-          position,
+          setTimeout(() => {
+            this.updateInitialRotation()
+          }, 200)
         })
-        this.setTimeout(() => {
-          this.updateInitialRotation()
-        }, 200)
-      })
+    })
   }
 
   @autobind
   updateInitialRotation() {
-    this.arNodeRef.getTransformAsync().then((retDict) => {
+    this.arNode.getTransformAsync().then((retDict) => {
       let rotation = retDict.rotation
       let absX = Math.abs(rotation[0])
       let absZ = Math.abs(rotation[2])
@@ -113,15 +118,17 @@ class InitialARScene extends React.Component {
   }
 
   render() {
+    const { position, scale, rotation, shouldBillboard } = this.state
+
     return (
       <ViroARScene ref={(ref) => (this.arScene = ref)}>
         <ViroAmbientLight color="#ffffff" intensity={200} />
         <ViroNode
-          transformBehaviors={this.state.shouldBillboard ? "billboardY" : []}
-          visible={this.props.arSceneNavigator.viroAppProps.displayObject}
-          position={this.state.objPosition}
-          scale={this.state.scale}
-          rotation={this.state.rotation}>
+          ref={(ref) => (this.arNode = ref)}
+          transformBehaviors={shouldBillboard ? "billboardY" : []}
+          position={position}
+          scale={scale}
+          rotation={rotation}>
           <ViroSpotLight
             innerAngle={5}
             outerAngle={20}
@@ -134,10 +141,11 @@ class InitialARScene extends React.Component {
             shadowOpacity={0.9}
           />
           <Viro3DObject
-            position={[0, 0.5, -1]}
+            position={[0, 0, -1]}
             source={require("./res/question_obj.obj")}
             resources={[require("./res/question.mtl")]}
             type="OBJ"
+            onLoadEnd={this.handleLoadEnd}
           />
           <ViroSurface
             rotation={[-90, 0, 0]}
